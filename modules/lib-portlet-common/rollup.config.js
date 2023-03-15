5e342dc3e8c6de09b74735caa40741a85b1acdf4
wsyski <wsyski@gmail.com>
2023-03-15 18:46:47 +0100

    Initial version

--- modules/lib-portlet-common/rollup.config.js
+++ modules/lib-portlet-common/rollup.config.js
@@ -1,42 +1,50 @@
-import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
-import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
-import dts from 'rollup-plugin-dts';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import del from 'rollup-plugin-delete';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from 'rollup-plugin-typescript2';
+import ttypescript from 'ttypescript';
+import pkg from './package.json';
 
-const packageJson = require('./package.json');
-
-export default [
-    {
-        input: 'src/index.ts',
-        output: [
-            {
-                file: packageJson.main,
-                format: 'cjs',
-                sourcemap: true,
-                name: 'react-ts-lib'
-            },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                sourcemap: true
-            }
-        ],
-        plugins: [
-            external(),
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: './tsconfig.json' }),
-            postcss(),
-            terser()
-        ],
-    },
-    {
-        input: 'dist/esm/types/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: "esm" }],
-        external: [/\.css$/],
-        plugins: [dts()],
-    },
-]
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            format: 'cjs',
+            dir: 'dist/cjs',
+            preserveModules: true,
+            preserveModulesRoot: 'src',
+            exports: 'named',
+        },
+        {
+            format: 'es',
+            dir: 'dist/esm',
+            preserveModules: true,
+            preserveModulesRoot: 'src',
+            exports: 'named',
+        },
+    ],
+    external: [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {}),
+        './src',
+    ],
+    plugins: [
+        peerDepsExternal(),
+        typescript({
+            typescript: ttypescript,
+            tsconfig: './tsconfig.json',
+        }),
+        nodeResolve(),
+        commonjs(),
+        babel({
+            babelHelpers: 'runtime',
+            exclude: 'node_modules/**',
+            extensions: ['.ts', '.tsx'],
+        }),
+        terser(),
+        del({ targets: 'dist/*' }),
+    ],
+};
