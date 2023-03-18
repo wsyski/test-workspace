ee097a1762905b403e6a1d395037cffe8441876b
wsyski <wsyski@gmail.com>
2023-03-18 13:11:08 +0100

    Initial version

--- modules/lib-portlet-common/rollup.config.js
+++ modules/lib-portlet-common/rollup.config.js
@@ -1,48 +1,30 @@
-import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
-import del from 'rollup-plugin-delete';
-import replace from 'rollup-plugin-replace';
-import typescript from 'rollup-plugin-typescript2';
-import ttypescript from 'ttypescript';
-import pkg from './package.json';
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-export default {
-    input: 'src/index.ts',
-    output: [
-        {
-            format: 'cjs',
-            dir: 'dist/cjs',
-            sourcemap: true
-        },
-        {
-            format: 'es',
-            dir: 'dist/esm',
-            sourcemap: true
-        },
-    ],
-    external: [
-        ...Object.keys(pkg.dependencies || {}),
-        ...Object.keys(pkg.devDependencies || {}),
-        ...Object.keys(pkg.peerDependencies || {}),
-        /node_modules/
-    ],
-    plugins: [
-        typescript({
-            typescript: ttypescript,
-            tsconfig: './tsconfig.json',
-        }),
-        commonjs(),
-        babel({
-            babelHelpers: 'runtime',
-            exclude: 'node_modules/**',
-            extensions: ['.ts', '.tsx'],
-        }),
-        replace({
-            ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-        }),
-        (process.env.NODE_ENV === 'production' && terser()),
-        del({ targets: 'dist/*' }),
-    ],
-};
+import pkg from './package.json' assert { type: 'json' };
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: pkg.main,
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: pkg.module,
+                format: "esm",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: "./tsconfig.json" }),
+        ],
+    }
+];
