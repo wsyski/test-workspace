131e11fc2584286b4982902015f9dd8d30a1ede1
wsyski <wsyski@gmail.com>
2023-03-18 16:36:28 +0100

    Initial version

--- modules/lib-portlet-common/rollup.config.js
+++ modules/lib-portlet-common/rollup.config.js
@@ -1,7 +1,10 @@
+import babel from '@rollup/plugin-babel';
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import terser from '@rollup/plugin-terser';
+import analyze from 'rollup-plugin-analyzer';
 
 import pkg from './package.json' assert { type: 'json' };
 
@@ -20,11 +23,27 @@ export default [
                 sourcemap: true,
             },
         ],
+        external: [
+            ...Object.keys(pkg.dependencies || {}),
+            ...Object.keys(pkg.devDependencies || {}),
+            ...Object.keys(pkg.peerDependencies || {})
+        ],
         plugins: [
-            peerDepsExternal(),
-            resolve(),
-            commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
+            peerDepsExternal(),
+            // resolve(),
+            commonjs(),
+            babel({
+                babelHelpers: 'runtime',
+                exclude: 'node_modules/**',
+                extensions: ['.ts', '.tsx'],
+            }),
+            (process.env.NODE_ENV === 'production' && terser()),
+            analyze({
+                hideDeps: true,
+                limit: 0,
+                summaryOnly: true,
+            })
         ],
     }
 ];
