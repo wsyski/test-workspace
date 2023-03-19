cd547f9ec44879d31be3bd27c08e85e500821118
wsyski <wsyski@gmail.com>
2023-03-19 12:37:15 +0100

    Initial version

--- modules/lib-portlet-common/rollup.config.mjs
+++ modules/lib-portlet-common/rollup.config.mjs
@@ -22,7 +22,6 @@ export default [
             },
         ],
         external: [
-            ...Object.keys(pkg.dependencies || {}),
             ...Object.keys(pkg.devDependencies || {}),
             ...Object.keys(pkg.peerDependencies || {})
         ],
