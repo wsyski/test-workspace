c61332a7534d0a3765f059cc817464db91574c67
wsyski <wsyski@gmail.com>
2023-03-27 15:48:04 +0200

    Initial version

---
+++ modules/frontend-js/packages/react-ts-portlet/src/index.js
@@ -0,0 +1,19 @@
+import { I18nUtil, LIFERAY_PARAMS_DEFAULT, LiferayUtil } from "@arena/lib-portlet-common";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { I18nextProvider } from 'react-i18next';
+
+import AppContainer from './AppContainer';
+import { PORTLET_INSTANCE_DEFAULT } from './constants/LiferayParamsConstants';
+const index = (liferayParams) => {
+    const liferayParamsWithDefaults = LiferayUtil.setLiferayParamsDefaults(liferayParams, PORTLET_INSTANCE_DEFAULT);
+    const i18nInstance = I18nUtil.init(liferayParamsWithDefaults.contextPath);
+    const markup = React.createElement(I18nextProvider, { i18n: i18nInstance }, React.createElement(AppContainer, {
+        liferayParams: liferayParamsWithDefaults,
+    }));
+    ReactDOM.render(process.env.NODE_ENV === 'development' ? (React.createElement(React.StrictMode, null, markup)) : (React.createElement(React.Fragment, null, markup)), document.getElementById(liferayParamsWithDefaults.portletElementId));
+};
+if (process.env.NODE_ENV === 'development') {
+    index(LIFERAY_PARAMS_DEFAULT);
+}
+export default index;
