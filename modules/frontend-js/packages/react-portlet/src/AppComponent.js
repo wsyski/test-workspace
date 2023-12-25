d3d9faf639bb97bf73e906c7c48c9b1aea812c76
wos <wos@axiell.com>
2023-12-25 17:17:21 +0100

    Upgrade Liferay GA 107

--- modules/frontend-js/packages/react-portlet/src/AppComponent.js
+++ modules/frontend-js/packages/react-portlet/src/AppComponent.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default function AppComponent(props) {
 	return (
@@ -7,26 +6,33 @@ export default function AppComponent(props) {
 			<div>
 				<span className="tag">
 					{Liferay.Language.get('portlet-namespace')}:
-				</span> 
+				</span>
+
 				<span className="value">{props.portletNamespace}</span>
 			</div>
+
 			<div>
 				<span className="tag">
 					{Liferay.Language.get('context-path')}:
-				</span> 
+				</span>
+
 				<span className="value">{props.contextPath}</span>
 			</div>
+
 			<div>
 				<span className="tag">
 					{Liferay.Language.get('portlet-element-id')}:
-				</span> 
+				</span>
+
 				<span className="value">{props.portletElementId}</span>
 			</div>
+
 			<div>
 				<span className="tag">
 					{Liferay.Language.get('configuration')}:
-				</span> 
-				<span className="value pre">{JSON.stringify(props.configuration, null, 2)}</span>
+				</span>
+
+				<span className="pre value">{JSON.stringify(props.configuration, null, 2)}</span>
 			</div>
 		</div>
 	);
