4c53ad9a0e01629d764dc92327af3cf3ca5b4e54
wos <wos@axiell.com>
2024-04-21 13:49:05 +0200

    Upgrade Liferay GA 111

---
+++ bin/build-portlet.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#set -x -v
+
+scriptDir=$(cd `dirname $0` && pwd)
+portlet=${1:-react-ts-portlet}
+echo "Building portlet: ${portlet} scriptDir: ${scriptDir}"
+cd ${scriptDir}/../modules/frontend-js
+yarn run clean
+yarn run reset:nx
+yarn run build:libs
+cd -
+cd ${scriptDir}/../modules/frontend-js/packages/react-provider
+yarn run clean
+yarn run build
+yarn run deploy
+cd -
+cd ${scriptDir}/../modules/frontend-js/packages/${portlet}
+yarn run clean
+yarn run build
+yarn run deploy
+cd -
