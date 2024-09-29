00e0c3e504448a124c3e9b417de5897cbd04315f
wos <wos@axiell.com>
2024-09-29 15:19:53 +0200

    Upgrade Liferay GA 125

--- client-extensions/vacation-test/src/App.jsx
+++ client-extensions/vacation-test/src/App.jsx
@@ -1,6 +1,5 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react'
+import viteLogo from './assets/vite.svg'
 import './App.css'
 import { sayHello, SimpleInput } from '@liferay/vacation-shared-code'
 
@@ -19,9 +18,6 @@ function App() {
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
