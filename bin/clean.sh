3e2ce533c532a1f4e7f8f0bc5a48c78daa1b6352
wos <wos@axiell.com>
2024-10-09 19:49:10 +0200

    Upgrade Liferay GA 125

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
-#set -v -x
-set -u -e
-
 scriptDir=$(cd `dirname $0` && pwd)
 echo "scriptDir: ${scriptDir}"
-find ${scriptDir}/.. -name yarn.lock -exec rm {} \;
+#find ${scriptDir}/.. -name yarn.lock -exec rm {} \;
 rm -rf ${scriptDir}/../node_modules_cache
 rm -rf ${scriptDir}/../modules/frontend-js/.nx
 find ${scriptDir}/.. -name .yarnrc -exec rm -rf {} \;
 find ${scriptDir}/.. -name build -exec rm -rf {} \;
-find ${scriptDir}/.. -name vite-build -exec rm -rf {} \;
 find ${scriptDir}/.. -name dist -exec rm -rf {} \;
 find ${scriptDir}/.. -name out -exec rm -rf {} \;
 find ${scriptDir}/.. -name out-tsc -exec rm -rf {} \;
 find ${scriptDir}/.. -name node_modules -exec rm -rf {} \;
 find ${scriptDir}/.. -name package-lock.json -exec rm {} \;
-find ${scriptDir}/.. -name yarn.lock -exec rm {} \;
 find ${scriptDir}/.. -name liferay-plugin.json -exec rm {} \;
 find ${scriptDir}/.. -name liferay-theme.json -exec rm {} \;
 find ${scriptDir}/.. -name liferay-npm-bundler-report.html -exec rm {} \;
