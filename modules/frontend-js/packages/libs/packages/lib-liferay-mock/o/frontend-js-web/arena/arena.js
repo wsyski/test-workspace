2e3e13f4f8e6dc90937432828aef3f87a48743ba
wos <wos@axiell.com>
2024-10-09 20:25:35 +0200

    Upgrade Liferay GA 125

--- modules/frontend-js/packages/libs/packages/lib-liferay-mock/o/frontend-js-web/arena/arena.js
+++ modules/frontend-js/packages/libs/packages/lib-liferay-mock/o/frontend-js-web/arena/arena.js
@@ -1,8 +1,6 @@
-(function(exports) {
-  "use strict";
-
-  /* theduck.axiell.com arenaMemberId: 30000 */
-  const patron = {
+(function (exports) {
+  'use strict';
+  var patron={
     arenaAgencyMemberId: "30000",
     arenaUserId: "1001",
     id: "F0767453EC1641F1E04400144FFB9119",
@@ -11,74 +9,60 @@
     roles: ["guest", "unrestricted"],
     token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlaHViQ29uc3VtZXJJZCI6MTM5NDAsImFyZW5hQWdlbmN5TWVtYmVySWQiOjMwMDAwLCJwaW4iOiIxMTExIiwiYXJlbmFVc2VySWQiOjEwMDEsInBhdHJvbklkIjoiRjA3Njc0NTNFQzE2NDFGMUUwNDQwMDE0NEZGQjkxMTkiLCJyb2xlcyI6WyJndWVzdCIsInVucmVzdHJpY3RlZCJdLCJpc3MiOiI0MDAwMCIsIm5hbWUiOiJBcmVuYSBQYXRyb24iLCJsaWJyYXJ5Q2FyZCI6IjExMTEiLCJpYXQiOjE3MDY5NjIyOTgsImJpcnRoRGF0ZSI6IjE5NTUtMDktMTMiLCJlbWFpbCI6Indvc0BheGllbGwuY29tIn0.FfffYKgCt-mG7m6Vti4sY-8qq16WPefmUyticsvWxP8"
   };
-
-  /* theduck.axiell.com arenaMemberId: 30000 */
-  /*
-  const patron = {
-    arenaAgencyMemberId: "30000",
-    arenaUserId: "1001",
-    id: "F0767453EC1641F1E04400144FFB9119",
-    name: "Arena Patron",
-    nick: "Arena",
-    roles: ["guest", "restricted"],
-    token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlaHViQ29uc3VtZXJJZCI6MTM5NDAsImFyZW5hQWdlbmN5TWVtYmVySWQiOjMwMDAwLCJwaW4iOiIxMTExIiwiYXJlbmFVc2VySWQiOjEwMDEsInBhdHJvbklkIjoiRjA3Njc0NTNFQzE2NDFGMUUwNDQwMDE0NEZGQjkxMTkiLCJyb2xlcyI6WyJndWVzdCIsInJlc3RyaWN0ZWQiXSwiaXNzIjoiNDAwMDAiLCJuYW1lIjoiQXJlbmEgUGF0cm9uIiwibGlicmFyeUNhcmQiOiIxMTExIiwiaWF0IjoxNzA2OTYyMzcyLCJiaXJ0aERhdGUiOiIxOTU1LTA5LTEzIiwiZW1haWwiOiJ3b3NAYXhpZWxsLmNvbSJ9.QyQohsSAX23C-kslMVth7xLU8vYF0_fk4DAjH0-3uh0"
-  };
-  */
-
-  /* theduck.axiell.com arenaMemberId: 30000 */
-  /*
-  const patron = {
-    arenaAgencyMemberId: "30000",
-    id: "patronId",
-    name: "Arena Patron",
-    roles: ["unrestricted"],
-    token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlaHViQ29uc3VtZXJJZCI6MTM5NDAsImFyZW5hQWdlbmN5TWVtYmVySWQiOjMwMDAwLCJwaW4iOiIxMTExIiwiYXJlbmFVc2VySWQiOjEwMDEsInBhdHJvbklkIjoiRjA3Njc0NTNFQzE2NDFGMUUwNDQwMDE0NEZGQjkxMTkiLCJyb2xlcyI6WyJ1bnJlc3RyaWN0ZWQiXSwiaXNzIjoiNDAwMDAiLCJuYW1lIjoiQXJlbmEgUGF0cm9uIiwibGlicmFyeUNhcmQiOiIxMTExIiwiaWF0IjoxNzA4MDg3MjY4LCJiaXJ0aERhdGUiOiIxOTU1LTA5LTEzIiwiZW1haWwiOiJ3b3NAYXhpZWxsLmNvbSJ9.GY1A64gxalw4qoD0LhZ-QJ_4gIUVCcJdYkqTb7foDjg"
-  };
-   */
-
-  /* theduck.axiell.com arenaMemberId: 30000 */
-  /*
-  const patron = {
-    arenaAgencyMemberId: "30000",
-    id: "patronId",
-    name: "Arena Patron",
-    roles: ["restricted"],
-    token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlaHViQ29uc3VtZXJJZCI6MTM5NDAsImFyZW5hQWdlbmN5TWVtYmVySWQiOjMwMDAwLCJhcmVuYVVzZXJJZCI6MTAwMSwicGF0cm9uSWQiOiJGMDc2NzQ1M0VDMTY0MUYxRTA0NDAwMTQ0RkZCOTExOSIsInJvbGVzIjpbInJlc3RyaWN0ZWQiXSwiaXNzIjoiNDAwMDAiLCJsaWJyYXJ5Q2FyZCI6IjExMTEiLCJiaXJ0aERhdGUiOiIxOTU1LTA5LTEzIiwicGluIjoiMTExMSIsIm5hbWUiOiJBcmVuYSBQYXRyb24iLCJleHAiOjE3MDgwMDE1NTMsImlhdCI6MTcwODAwMTU1MiwiZW1haWwiOiJ3b3NAYXhpZWxsLmNvbSJ9.zuvRtsO8vqF1DEqodMpQmjv66ELmvUtqmC--nboXAgA"
-  };
-   */
-
-  const localApiEndpoint = "http://arena-local:16520/local-rest/api/v1";
-
+  var maxInactiveInterval = 900;
+  var localApiEndpoint = "http://arena-local:16520/local-rest/api/v1";
+  var federatedSearchApiEndpoint = "http://arena-federated-search:9799";
+  var transactionApiEndpoint = "http://arena-transaction:9610";
   function isSignedIn() {
     return !!patron;
   }
-
-  function getLocalApiEndpoint() {
-    const pathname = new URL(localApiEndpoint).pathname;
-    const port = location.port;
-    const hostname = location.hostname;
+  function getMaxInactiveInterval() {
+    return maxInactiveInterval;
+  }
+  function getApiEndpoint(localhostApiEndpoint, proxyPathname) {
+    var localhostPathname = (new URL(localhostApiEndpoint)).pathname;
+    if (localhostPathname.endsWith('/')) {
+      localhostPathname = localhostPathname.slice(0, -1);
+    }
+    var localhostPort = (new URL(localhostApiEndpoint)).port;
+    var localhostProtocol = (new URL(localhostApiEndpoint)).protocol;
+    var port = location.port;
+    var hostname = location.hostname;
     switch (port) {
       case undefined:
-      case "":
-      case "80":
-      case "443":
-        return "//" + hostname + pathname;
-      case "3000":
-      case "6080":
-        return "//theduck.axiell.com:16520" + pathname;
-      case "16519":
-        return "//" + hostname + ":16520" + pathname;
+      case '':
+      case '80':
+      case '443':
+        return proxyPathname;
+      case '3000':
+      case '6080':
+      case '8080':
+      case '16519':
+        return localhostProtocol === 'https' ? localhostApiEndpoint : '//' + hostname + ':' + localhostPort + localhostPathname;
       default:
-        return localApiEndpoint;
+        return localhostApiEndpoint;
     }
   }
-
+  function getLocalApiEndpoint() {
+    return getApiEndpoint(localApiEndpoint, '/local-rest/api/v1');
+  }
+  function getFederatedSearchApiEndpoint() {
+    return getApiEndpoint(federatedSearchApiEndpoint, '/federated-search');
+  }
+  function getTransactionApiEndpoint() {
+    return getApiEndpoint(transactionApiEndpoint, '/transaction');
+  }
   function getPatron() {
     return patron;
   }
-
+  function setPatron(p) {
+    patron = p;
+  }
   exports.isSignedIn = isSignedIn;
+  exports.getMaxInactiveInterval = getMaxInactiveInterval;
   exports.getLocalApiEndpoint = getLocalApiEndpoint;
+  exports.getFederatedSearchApiEndpoint = getFederatedSearchApiEndpoint;
+  exports.getTransactionApiEndpoint = getTransactionApiEndpoint;
   exports.getPatron = getPatron;
-})((window.Arena = window.Arena || {}));
-
+  exports.setPatron = setPatron;
+} (window.Arena = window.Arena || {}));
