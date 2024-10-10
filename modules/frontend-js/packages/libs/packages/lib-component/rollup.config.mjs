356f20750aba4aab40955a2373b445fec32e97d7
wos <wos@axiell.com>
2024-10-10 20:24:40 +0200

    Upgrade Liferay GA 125

---
+++ modules/frontend-js/packages/libs/packages/lib-component/rollup.config.mjs
@@ -0,0 +1,49 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from "@rollup/plugin-commonjs";
+import terser from '@rollup/plugin-terser';
+import {readFileSync} from "node:fs";
+import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+const packageJson = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf-8'));
+const input = 'out-tsc/index.js';
+const external = ['node_modules', ...Object.keys(packageJson.dependencies || {}), ...Object.keys(packageJson.peerDependencies || {})];
+
+const getPlugins = () => {
+
+    return [
+        peerDepsExternal(),
+        commonjs(),
+        babel(),
+        postcss(),
+        (process.env.NODE_ENV === 'production' && terser())
+    ];
+};
+
+
+export default [
+    {
+        external,
+        input,
+        output: [
+            {
+                file: packageJson.main,
+                format: 'cjs',
+                sourcemap: true,
+            },
+            {
+                file: packageJson.module,
+                format: "esm",
+                sourcemap: true,
+            }
+        ],
+        plugins: getPlugins()
+    },
+    {
+        external: [/\.(css|less|scss)$/],
+        input: "dist/esm/types/index.d.ts",
+        output: [{file: "dist/index.d.ts", format: "esm"}],
+        plugins: [dts()]
+    },
+];
