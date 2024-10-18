8c0e2af973bb69704be91b14f34374392357aeae
wos <wos@axiell.com>
2024-10-18 11:46:28 +0200

    Upgrade Liferay GA 125

--- modules/frontend-js/packages/k10-search-portlet/src/index.js
+++ modules/frontend-js/packages/k10-search-portlet/src/index.js
@@ -1,5 +1,37 @@
-import {LIFERAY_PARAMS_DEFAULT} from '@arena/lib-portlet-common';
+import {I18nUtil, LiferayUtil} from '@arena/lib-portlet-common';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {I18nextProvider} from 'react-i18next';
 
-import main from './main';
+import AppContainer from './AppContainer';
+import {PORTLET_INSTANCE_DEFAULT} from './constants/LiferayParamsConstants';
 
-main(LIFERAY_PARAMS_DEFAULT);
+const index = (liferayParams) => {
+	const liferayParamsWithDefaults = LiferayUtil.setLiferayParamsDefaults(
+		liferayParams,
+		PORTLET_INSTANCE_DEFAULT
+	);
+	const i18nInstance = I18nUtil.init(liferayParamsWithDefaults.contextPath, liferayParamsWithDefaults.configuration.portletInstance[CUSTOM_TRANSLATION]);
+
+	const markup = React.createElement(
+		I18nextProvider,
+		{i18n: i18nInstance},
+		React.createElement(AppContainer, {
+			liferayParams: liferayParamsWithDefaults,
+		})
+	);
+	ReactDOM.render(
+		process.env.NODE_ENV === 'development' ? (
+			<React.StrictMode>{markup}</React.StrictMode>
+		) : (
+			<React.Fragment>{markup}</React.Fragment>
+		),
+		document.getElementById(liferayParamsWithDefaults.portletElementId)
+	);
+};
+
+if (process.env.NODE_ENV === 'development') {
+	index(LIFERAY_PARAMS_DEFAULT);
+}
+
+export default index;
