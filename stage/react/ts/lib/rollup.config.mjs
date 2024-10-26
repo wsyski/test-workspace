f22c37fe9fadaaca107ff27077d447fddefce397
wos <wos@axiell.com>
2024-10-26 08:59:30 +0200

    Upgrade Liferay GA 125

--- stage/react/ts/lib/rollup.config.mjs
+++ stage/react/ts/lib/rollup.config.mjs
@@ -1,20 +1,22 @@
 import commonjs from "@rollup/plugin-commonjs";
-import terser from '@rollup/plugin-terser';
+import resolve from "@rollup/plugin-node-resolve";
+import json from '@rollup/plugin-json';
+import terser from "@rollup/plugin-terser";
 import {readFileSync} from "node:fs";
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import typescript from "rollup-plugin-typescript2";
-
-
 
 const packageJson = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf-8'));
 
 const getPlugins = () => {
 
     return [
-        peerDepsExternal(),
-        commonjs(),
         typescript({tsconfig: "./tsconfig.json"}),
+        // json(),
+        peerDepsExternal(),
+        // resolve(),
+        commonjs(),
         (process.env.NODE_ENV === 'production' && terser())
     ];
 };
