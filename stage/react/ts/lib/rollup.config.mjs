62ab0a7c9eb22bd169e172861295074bdcd2ba01
wos <wos@axiell.com>
2024-10-28 15:58:30 +0100

    Upgrade Liferay GA 125

--- stage/react/ts/lib/rollup.config.mjs
+++ stage/react/ts/lib/rollup.config.mjs
@@ -1,17 +1,23 @@
+import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import json from '@rollup/plugin-json';
-import terser from "@rollup/plugin-terser";
-import {readFileSync} from "node:fs";
 import dts from "rollup-plugin-dts";
+import json from '@rollup/plugin-json';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import replace from '@rollup/plugin-replace';
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 import typescript from "rollup-plugin-typescript2";
+import {readFileSync} from "node:fs";
 
 const packageJson = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf-8'));
 
 const getPlugins = () => {
 
     return [
+        replace({
+            preventAssignment: true,
+            'process.env.NODE_ENV': JSON.stringify('development'),
+        }),
         typescript({tsconfig: "./tsconfig.json"}),
         // json(),
         peerDepsExternal(),
@@ -30,12 +36,15 @@ export default [
             {
                 file: packageJson.main,
                 format: 'cjs',
+                interop: 'auto',
                 sourcemap: true,
+                globals: { react: 'React' }
             },
             {
                 file: packageJson.module,
                 format: "esm",
                 sourcemap: true,
+                globals: { react: 'React' }
             }
         ],
         plugins: getPlugins()
