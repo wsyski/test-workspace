aaf6e910e6c8845211d1910bb5ef3a6afa7ce8ca
wos <wos@axiell.com>
2025-03-13 14:55:23 +0100

    Upgrade Liferay GA 132

---
+++ modules/frontend-js/packages/react-ts-portlet/src/index.js
@@ -0,0 +1,3 @@
+import index from '@arena/lib-react-ts-portlet';
+
+export default index;
--- stage/react/ts/main/jest.polyfills.js
+++
@@ -1,9 +0,0 @@
-const { TextDecoder, TextEncoder, ReadableStream } = require("node:util")
-
-Object.defineProperties(globalThis, {
-    ReadableStream: { value: ReadableStream },
-    TextDecoder: { value: TextDecoder },
-    TextEncoder: { value: TextEncoder }
-})
-
-
