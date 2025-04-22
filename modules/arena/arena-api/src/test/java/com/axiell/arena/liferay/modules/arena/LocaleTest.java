f2ab0026650dcc7b78cdd5ad04897735767997fd
wos <wos@axiell.com>
2025-04-22 14:11:32 +0200

    Upgrade to Liferay 7.4.3.125-ga125

--- modules/arena/arena-api/src/test/java/com/axiell/arena/liferay/modules/arena/LocaleTest.java
+++ modules/arena/arena-api/src/test/java/com/axiell/arena/liferay/modules/arena/LocaleTest.java
@@ -1,5 +1,6 @@
 package com.axiell.arena.liferay.modules.arena;
 
+import lombok.extern.java.Log;
 import org.junit.jupiter.api.Test;
 import java.util.Calendar;
 import java.util.Locale;
@@ -7,12 +8,14 @@ import java.util.Locale;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+@Log
 public class LocaleTest {
 
     @Test
     public void firstDayOfTheWeek() {
         Locale locale = new Locale("en", "GB");
         Calendar calobj = Calendar.getInstance(locale);
+        System.out.println("First day of the week: " + calobj.getFirstDayOfWeek());
         assertThat(calobj.getFirstDayOfWeek(), is(2));
     }
 }
